c0d0bfc2ff26f44cea561b5094869a79d7010ab9
kanaga12345 <kanaga9706@gmail.com>
2025-01-22 20:47:39 +0530

    QA

--- src/test/java/org/execution/TestScript.java
+++ src/test/java/org/execution/TestScript.java
@@ -115,7 +115,8 @@ public class TestScript extends BaseClass {
 
         WebElement clickLogin = driver.findElement(By.xpath("//a[text()='Click here to login again']"));
         baseClass.clickButton(clickLogin);
-        
+        System.out.println("Hi Work Done by QA");
+        System.out.println("QA Work");
         
         
         
